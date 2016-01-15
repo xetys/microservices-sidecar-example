acd8ecca7c5a0f25e1baaca75933a39891837bef
David Steiman <d.steiman@ktel.de>
2016-01-15 18:46:55 +0100

    sidecar implementation for rails apps - not really working yet

---
+++ sidecar/src/main/java/de/ktel/microservices/SidecarApplication.java
@@ -0,0 +1,18 @@
+package de.ktel.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+
+/**
+ * Created on 15.01.16.
+ *
+ * @author David Steiman, K-TEL Communications GmbH
+ */
+@SpringBootApplication
+@EnableSidecar
+public class SidecarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SidecarApplication.class, args);
+    }
+}
